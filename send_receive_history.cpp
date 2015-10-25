ed7032189d72434c2852875afbae5828927eb9cb
ottyajp <ottyajp@gmail.com>
2015-10-25 23:17:24 +0900

    受取履歴を見れるように

--- send_receive_history.cpp
+++ send_receive_history.cpp
@@ -50,6 +50,42 @@ void send_receive_history::on_reload_clicked()
 
     for(int i=0;i<tx_send_receive_limit.toInt();i++){
         QJsonObject json = send_json.object().value("transactions").toArray().at(i).toObject();
+        if(json.value("created").toInt() == 0){break;}
+        QString created = from_unix_time(json.value("created").toInt());
+        QString type = json.value("item").toString();
+        if(type == "send") type = tr("send");
+        QString amount = QString::number(
+                    json.value("amount").toString().toDouble() / 100000000,'f',8)
+                .replace(QRegularExpression("[0]*$"),"").replace(QRegularExpression("\\.$"),"");
+        //anonymous user topic res text
+        QString anonymous = QString::number(json.value("anonymous").toInt());
+        if(anonymous == "1"){
+            anonymous = tr("anonymous");
+        }else{
+            anonymous = tr("known");
+        }
+        QString user = QString::number(json.value("user").toObject().value("u_id").toInt());
+        QString topic,res;
+        if(json.value("t_id").toInt() != 0){
+            topic = QString::number(json.value("t_id").toInt());
+            res = QString::number(json.value("r_id").toInt());
+        }
+        QString text;
+        json.value("msg_text").toString();
+        QTreeWidgetItem *item = new QTreeWidgetItem();
+        item->setText(0,created);
+        item->setText(1,type);
+        item->setText(2,amount);
+        item->setText(3,anonymous);
+        item->setText(4,user);
+        item->setText(5,topic);
+        item->setText(6,res);
+        item->setText(7,text);
+        ui->view->addTopLevelItem(item);
+    }
+
+    for(int i=0;i<tx_send_receive_limit.toInt();i++){
+        QJsonObject json = receive_json.object().value("transactions").toArray().at(i).toObject();
         qDebug()<<json.value("created").toInt();
         if(json.value("created").toInt() == 0){break;}
         QString created = from_unix_time(json.value("created").toInt());
