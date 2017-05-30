6cd1b54b2ccafa3be4f25368f6d66a704cf8a518
ottyajp <ottyajp@gmail.com>
2017-05-31 00:23:01 +0900

    終了ボタン追加、ついでにWebViewのテストとAPIアクセスのテスト
    無意味にconfigも今回つけてみた

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,27 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "network_access.h"
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    QWebView *view = new QWebView();
+    view->setUrl(QUrl("http://google.com/"));
+    ui->topic_tab_widget->addTab(view, "test");
+//    QUrlQuery query;
+//    QJsonDocument json = QJsonDocument::fromJson(access_get("topics/list",query).toUtf8());
+//    qDebug()<<json;
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_actionQuit_Q_triggered()
+{
+    QApplication::quit();
+    QCoreApplication::quit();
 }
