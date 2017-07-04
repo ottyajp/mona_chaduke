276764b1323fa622e9f3fb7dfee551453f9e1de2
ottyajp <ottyajp@gmail.com>
2017-07-04 18:28:01 +0900

    和訳

--- ja.ts
+++ ja.ts
@@ -9,74 +9,146 @@
         <translation>もなちゃづけ</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="36"/>
+        <location filename="mainwindow.ui" line="38"/>
         <source>ID</source>
         <translation>トピックID</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="41"/>
+        <location filename="mainwindow.ui" line="43"/>
         <source>Rank</source>
         <translation>順位</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="46"/>
+        <location filename="mainwindow.ui" line="48"/>
         <source>Topic title</source>
         <translation>タイトル</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="51"/>
+        <location filename="mainwindow.ui" line="53"/>
         <source>Count</source>
         <translation>レス数</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="56"/>
+        <location filename="mainwindow.ui" line="58"/>
         <source>Updated</source>
         <translation>浮上日</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="61"/>
+        <location filename="mainwindow.ui" line="63"/>
         <source>Created</source>
         <translation>作成日</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="66"/>
+        <location filename="mainwindow.ui" line="68"/>
         <source>Category</source>
         <translation>カテゴリ</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="71"/>
+        <location filename="mainwindow.ui" line="73"/>
         <source>Tags</source>
         <translation>タグ</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="98"/>
+        <location filename="mainwindow.ui" line="97"/>
         <source>&amp;File</source>
         <translation>ファイル (&amp;F)</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="106"/>
+        <location filename="mainwindow.ui" line="105"/>
         <source>&amp;Topic</source>
         <translation>トピック (&amp;T)</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="124"/>
+        <location filename="mainwindow.ui" line="123"/>
         <source>Config (&amp;C)</source>
         <translation>コンフィグ (&amp;C)</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="129"/>
+        <location filename="mainwindow.ui" line="128"/>
         <source>Quit (&amp;Q)</source>
         <translation>終了 (&amp;Q)</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="132"/>
+        <location filename="mainwindow.ui" line="131"/>
         <source>Ctrl+Q</source>
         <translation>Ctrl+Q</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="137"/>
+        <location filename="mainwindow.ui" line="136"/>
         <source>&amp;Load topic list</source>
         <translation>トピックリストをロード (&amp;L)</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="topic_view.cpp" line="64"/>
+        <location filename="topic_view.cpp" line="67"/>
+        <source>people</source>
+        <translation>人</translation>
+    </message>
+    <message>
+        <location filename="topic_view.cpp" line="79"/>
+        <source>send mona</source>
+        <translation>←送る</translation>
+    </message>
+</context>
+<context>
+    <name>config</name>
+    <message>
+        <location filename="config.ui" line="14"/>
+        <source>Dialog</source>
+        <translation>コンフィグ</translation>
+    </message>
+    <message>
+        <location filename="config.ui" line="26"/>
+        <source>Monacoin Address</source>
+        <translation>Monacoinアドレス</translation>
+    </message>
+    <message>
+        <location filename="config.ui" line="50"/>
+        <source>Password</source>
+        <translation>パスワード</translation>
+    </message>
+    <message>
+        <location filename="config.ui" line="87"/>
+        <source>not authenticated</source>
+        <translation>未認証</translation>
+    </message>
+    <message>
+        <location filename="config.ui" line="123"/>
+        <source>Authenticate</source>
+        <translation>認証する</translation>
+    </message>
+    <message>
+        <location filename="config.ui" line="167"/>
+        <source>Get Topics limit</source>
+        <translation>トピック取得数</translation>
+    </message>
+    <message>
+        <location filename="config.ui" line="191"/>
+        <source>Get Responses limit</source>
+        <translation>レス取得数</translation>
+    </message>
+    <message>
+        <location filename="config.cpp" line="23"/>
+        <source>you must fill Monacoin address.</source>
+        <translation>Monacoinアドレスを入力してください。</translation>
+    </message>
+    <message>
+        <location filename="config.cpp" line="28"/>
+        <source>you must fill your Passowrd.</source>
+        <translation>パスワードを入力してください。</translation>
+    </message>
+    <message>
+        <location filename="config.cpp" line="41"/>
+        <source>authentication failed.</source>
+        <translation>認証に失敗しました。</translation>
+    </message>
+    <message>
+        <location filename="config.cpp" line="72"/>
+        <source>invalid user</source>
+        <translation>無効なユーザ</translation>
     </message>
 </context>
 </TS>
